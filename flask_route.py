1de027214e38970210d2a08ba582a545d2cb25fa
zch634308491 <zch_634308491@aliyun.com>
2022-07-12 11:32:45 +0800

    flask demo up

---
+++ flask_route.py
@@ -0,0 +1,12 @@
+# -*- coding:UTF-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    # app.run()
+    app.run(host='192.168.123.23')
